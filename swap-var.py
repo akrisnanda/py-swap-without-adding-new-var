bf19e1caacc4c65fb2e67cccc48b4b0bacd5102f
akrisnanda <44166275+akrisnanda@users.noreply.github.com>
2023-07-14 17:30:04 +0700

    Add files via upload

---
+++ swap-var.py
@@ -0,0 +1,13 @@
+print('Enter x:')
+x = int(input())
+
+print('Enter y:')
+y = int(input())
+
+print("X:"+ str(x) +" Y:"+ str(y))
+
+x = x + y
+y = x - y
+x = x - y
+
+print("X:"+ str(x) +" Y:"+ str(y))
